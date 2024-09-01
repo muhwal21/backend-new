3dc2ca19186a5680d67086e1477a11325140ab4b
muhwal21 <muh.awaluddin.h1@gmail.com>
2024-09-01 22:03:32 +0700

    saa

--- api/create-task.js
+++ api/create-task.js
@@ -1,7 +1,7 @@
 import { IncomingForm } from 'formidable';
 import fs from 'fs';
-import fetch from 'node-fetch'; // Pastikan `node-fetch` adalah versi yang benar
-import FormData from 'form-data'; // Pastikan ini terpasang di dependencies
+import fetch from 'node-fetch';
+import FormData from 'form-data';
 
 export const config = {
     api: {
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    
+
     if (req.method === 'OPTIONS') {
         res.status(200).end();
         return;
